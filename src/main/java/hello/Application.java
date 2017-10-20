80edc716c6f508023cfb3a908fa15bd94e6fc0b2
Michael Schwartz <mschwartz@collab.net>
2017-10-20 01:03:55 -0500

    [artf1234] - fixing the build

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,8 +1,6 @@
 package hello;
 
 import java.util.Arrays;
-foo bar
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
